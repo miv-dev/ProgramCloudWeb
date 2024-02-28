080d8f23f1e42e0b663a6c65b61b6323a580a4b7
miv-dev <i@mike39.ru>
2024-02-28 17:58:27 +0300

    Fix bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import LoginPage from './pages/Login';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import RequireAuth from './helpers/RequireAuth';
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<HomePage />}/>
-          <Route path='login' element={ <LoginPage/> } />
-          {/*<Route element={<RequireAuth/>}>*/}
+    return (
+        <>
+            <Routes>
+                <Route path='/' element={<Layout/>}>
+                    <Route path='login' element={<LoginPage/>}/>
+                    <Route element={<RequireAuth/>}>
+                        <Route index element={<HomePage/>}/>
 
-          {/*</Route>*/}
-        </Route>
-      </Routes>
-    </>
-  );
+                    </Route>
+                </Route>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
